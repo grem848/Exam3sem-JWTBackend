f64b2143f89112ca9f27a403420d6818aaa6eada
lars-m <lam@cphbusiness.dk>
2018-11-01 18:06:13 +0100

    Startcode for the fall-2018 security exercise

--- src/main/java/security/SharedSecret.java
+++ src/main/java/security/SharedSecret.java
@@ -11,14 +11,15 @@ public class SharedSecret {
     public static byte[] getSharedKey() {
         System.out.println("******************* IMPORTANT ******************'");
         System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-        System.out.println("**** REMOVE FIXED SECRET BEFORE PRODUCTION ******");
+        System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        System.out.println("**** REMOVE FIXED SECRET BEFORE PRODUCTION *******");
+        System.out.println("****      See security.SharedSecret        *******");
         System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
         System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
         //REMOVE BEFORE PRODUCTION
         if(true){
             return "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA".getBytes();
         }
-        
         if (secret == null) {
             secret = new byte[32];
             new SecureRandom().nextBytes(secret);
